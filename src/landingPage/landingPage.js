f1ff2a49d85e3e19c7326290ba6973586c67390a
brunovenegas <bruno.venegas@gmail.com>
2017-11-14 08:11:36 -0800

    Created new branch where I will be creating poll creating feature

--- src/landingPage/landingPage.js
+++ src/landingPage/landingPage.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Alert } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 const util = require('util');
+const whatCanDemocracy = 'What can democracy do for you?';
 
 export default class LandingPage extends Component {
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'Democratize This!',
   };
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text> Landing Page </Text>
+        <Text style={styles.subtitle}>whatCanDemocracy</Text>
+        <TextInput style={styles.textInput}>Hello hello 123</TextInput>
+        <Text style={styles.subtitle}>Choices</Text>
+        <TextInput style={styles.textInput}>Let us try this again</TextInput>
       </View>
     );
   }
@@ -19,9 +23,19 @@ export default class LandingPage extends Component {
 
 const styles = StyleSheet.create({
     wrapper: {
-    backgroundColor: '#FFFFFF',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#3498db',
+    flex: 1
+  },
+  subtitle: {
+    color: 'white',
+    fontWeight: '200'
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    color: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 10
   }
 });
